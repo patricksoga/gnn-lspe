4ab9eb1784bd59553efb98aca48a11950faf5ac5
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-08-31 03:48:32 -0400

    fix

---
+++ tests/MOLTOX21/gape/b128-ngape3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#$ -N LSPE-gape-b128
+#$ -q gpu
+#$ -l gpu_card=1
+#$ -t 1-5:1
+
+pos_enc_dim=(0 4 8 16 32 64)
+fname=$(pwd)/b128-bnorm-alt-edge_${SGE_TASK_ID}_${pos_enc_dim[${SGE_TASK_ID}]}_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/gnn-lspe
+
+python main_ZINC_graph_regression.py --config ../configs/GatedGCN_MOLTOX21_LSPE.json --pe_init gape --pos_enc_dim ${pos_enc_dim[${SGE_TASK_ID}]}
