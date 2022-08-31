145aa89d688c3ee45417d710c466eb6154c1f746
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-08-31 16:57:00 -0400

    tests

--- tests/ZINC/GatedGCN/gape/lspe-eigloss-ngape1-eq/GatedGCN_ZINC_lspe-eigloss-ngape1-eq_4.sh
+++ tests/ZINC/GatedGCN/gape/lspe-eigloss-ngape1-eq/GatedGCN_ZINC_lspe-eigloss-ngape1-eq_4.sh
@@ -8,6 +8,6 @@ touch $fname
 fsync -d 10 $fname &
 
 conda activate gnn
-cd /afs/crc.nd.edu/user/p/psoga/benchmarking-gnns
+cd /afs/crc.nd.edu/user/p/psoga/gnn-lspe
 
 python3 main_ZINC_graph_regression.py --config tests/test-configs/GatedGCN_ZINC_lspe-eigloss-ngape1-eq.json --job_num 4 --pos_enc_dim 4 --log_file $fname
