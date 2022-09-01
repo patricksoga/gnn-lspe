7feec367d3d9a3272914bc55f2be400f280d2955
Patrick Soga <psoga@psoga.dhcp.nd.edu>
2022-08-31 21:52:27 -0400

    tests

---
+++ tests/ZINC/GatedGCN/gape/gatedgcn_lspe_gape/GatedGCN_ZINC_gatedgcn_lspe_gape_24.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#$ -N GatedGCN_ZINC_gatedgcn_lspe_gape_24
+#$ -q gpu
+#$ -l gpu_card=1
+
+fname=$(pwd)/24_DEBUG.log
+touch $fname
+fsync -d 10 $fname &
+
+conda activate gnn
+cd /afs/crc.nd.edu/user/p/psoga/gnn-lspe
+
+python3 main_ZINC_graph_regression.py --config tests/test-configs/GatedGCN_ZINC_gatedgcn_lspe_gape.json --job_num 24 --pos_enc_dim 24 --log_file $fname
